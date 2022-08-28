dd6d6896e3fe176b806887eb6b8628d0db182ed9
omismone <simone.migliazza1@gmail.com>
2022-08-28 23:55:03 +0200

    bug fix controller

--- hermes/web-project/src/main/java/com/omismone/strapizzami/controller/PresentationController.java
+++ hermes/web-project/src/main/java/com/omismone/strapizzami/controller/PresentationController.java
@@ -1,14 +1,5 @@
 package com.omismone.strapizzami.controller;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 public class PresentationController {
 	private static PresentationController singleton;
@@ -23,28 +14,11 @@ public class PresentationController {
 	}
 	
 	/**
-	 * 
-	 * @param request the page needed
-	 * @return
+	 * @return menu html
 	 */
-	public String getPage(String request) {
-		String response = "";
+	public String getMenu() {
+		String content = "";
 		
-
-        InputStream input = this.getClass().getResourceAsStream("html/" + request + ".html");
-
-		try {
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(input));
-            String line;
-            while ((line = buffer.readLine()) != null) {
-                response += line;
-            }
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return response;
+		return content;
 	}
 }
--- hermes/web-project/src/main/java/com/omismone/strapizzami/view/menu.java
+++ hermes/web-project/src/main/java/com/omismone/strapizzami/view/menu.java
@@ -27,7 +27,7 @@ public class menu extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append(PresentationController.getInstance().getPage("menu"));
+		response.getWriter().append(PresentationController.getInstance().getMenu());
 	}
 
 	/**
@@ -38,33 +38,5 @@ public class menu extends HttpServlet {
 		doGet(request, response);
 	}
 	
-	public static String getBaseURL(HttpServletRequest req) {
-
-	    String scheme = req.getScheme();             // http
-	    String serverName = req.getServerName();     // hostname.com
-	    int serverPort = req.getServerPort();        // 80
-	    String contextPath = req.getContextPath();   // /mywebapp
-	    String servletPath = req.getServletPath();   // /servlet/MyServlet
-	    String pathInfo = req.getPathInfo();         // /a/b;c=123
-	    String queryString = req.getQueryString();          // d=789
-
-	    // Reconstruct original requesting URL
-	    StringBuilder url = new StringBuilder();
-	    url.append(scheme).append("://").append(serverName);
-
-	    if (serverPort != 80 && serverPort != 443) {
-	        url.append(":").append(serverPort);
-	    }
-	    /*
-	    url.append(contextPath).append(servletPath);
-
-	    if (pathInfo != null) {
-	        url.append(pathInfo);
-	    }
-	    if (queryString != null) {
-	        url.append("?").append(queryString);
-	    }*/
-	    return url.toString();
-	}
 
 }
