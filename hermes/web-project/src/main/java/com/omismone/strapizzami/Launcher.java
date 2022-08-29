0df9a59fed910611998b5ebe83f470333a21dd34
omismone <simone.migliazza1@gmail.com>
2022-08-29 19:38:29 +0200

    renamed tester

--- hermes/web-project/src/main/java/com/omismone/strapizzami/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.omismone.strapizzami;
-
-import com.omismone.strapizzami.services.PersistenceFacade;
-
-public class Launcher {
-    public static void main(String[] args){
-        PersistenceFacade.getInstance().getDishes().forEach(x -> {System.out.println(x.getName());});
-    }
-}
---
+++ hermes/web-project/src/main/java/com/omismone/strapizzami/Tester.java
@@ -0,0 +1,9 @@
+package com.omismone.strapizzami;
+
+//import com.omismone.strapizzami.services.PersistenceFacade;
+
+public class Tester {
+    public static void main(String[] args){
+        //PersistenceFacade.getInstance().getDishes().forEach(x -> {System.out.println(x.getName());});
+    }
+}
